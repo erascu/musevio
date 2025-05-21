88e7d42c63f7b1cee488e6878949dde3c8e834fa
erascu <e.rashku@gmail.com>
2025-05-21 15:22:45 +0300

    create custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import { Container } from "@/components/shared";
+import Image from "next/image";
+
+function NotFound() {
+  return (
+    <Container className="px-4 pt-30 flex justify-center items-center flex-col md:flex-row">
+      <Image
+        src="/404.svg"
+        alt="Not found"
+        width={200}
+        height={189}
+        className="max-w-[180px] md:max-w-[200px]"
+      />
+      <div className="md:max-w-[400px] max-w-[300px] mt-3 md:ml-7">
+        <h1 className="playfair font-bold md:!text-6xl !text-4xl text-center md:text-left">
+          This page doesn’t exist
+        </h1>
+        <p className="mt-3 text-ring max-w-[300px] md:text-left text-center">
+          Not all masterpieces can be found. Use the menu above to navigate.
+        </p>
+      </div>
+    </Container>
+  );
+}
+
+export default NotFound;
