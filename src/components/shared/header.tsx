0bc753c9277d2a99ffa91e1b3dfde367c2da1c7f
erascu <e.rashku@gmail.com>
2025-05-21 15:21:41 +0300

    update header nav menu, breakpoints

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,28 +1,32 @@
+"use client";
 import { cn } from "@/lib/utils";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { Container } from "./index";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import { ChevronRight, Heart } from "lucide-react";
+import { ChevronRight, Heart, Menu } from "lucide-react";
 
 interface Props {
   className?: string;
 }
 
 interface LinkProps {
-  link: string;
+  href: string;
   title: string;
 }
 
 export const Header: React.FC<Props> = ({ className }) => {
   const links: LinkProps[] = [
-    { link: "", title: "Home" },
-    { link: "", title: "Antiquities" },
-    { link: "", title: "Fine Art" },
-    { link: "", title: "About" },
-    { link: "", title: "Contacts" },
+    { href: "/", title: "Home" },
+    { href: "/antiquities", title: "Antiquities" },
+    { href: "/fineart", title: "Fine Art" },
+    { href: "/about", title: "About" },
+    { href: "/contacts", title: "Contacts" },
   ];
+
+  const pathname = usePathname();
 
   return (
     <header className={cn("", className)}>
@@ -38,31 +42,45 @@ export const Header: React.FC<Props> = ({ className }) => {
           />
         </Link>
         <div className="flex items-center">
-          <nav>
+          <Link href="" className="lg:hidden ">
+            <Menu className="text-wine" size={30} />
+          </Link>
+          <nav className="hidden lg:block">
             <ul className="flex items-center">
-              {links.map((link, i) => (
+              {links.map((item, i) => (
                 <li className="flex items-center relative" key={i}>
                   <Link
-                    href=""
-                    className="inline-block mr-10 text-primary hover:text-wine text-lg group text-nowrap"
+                    href={item.href}
+                    className={`inline-block mr-10 text-primary hover:text-wine text-lg group text-nowrap ${
+                      pathname === item.href && "text-wine"
+                    }`}
                   >
-                    {link.title}
-                    <ChevronRight
-                      size={16}
-                      className="absolute left-[-20px] top-1/2 -translate-y-1/2 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 text-[#801336] pointer-events-none"
-                    />
+                    {item.title}
+                    {item.href === pathname ? (
+                      <ChevronRight
+                        size={16}
+                        className="text-wine absolute left-[-20px] top-1/2 -translate-y-1/2"
+                      />
+                    ) : (
+                      <ChevronRight
+                        size={16}
+                        className="absolute left-[-20px] top-1/2 -translate-y-1/2 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 text-wine pointer-events-none"
+                      />
+                    )}
                   </Link>
                 </li>
               ))}
             </ul>
           </nav>
-          <Button
-            variant="default"
-            className="flex items-center text-lg font-[300]"
-          >
-            <Heart size={18} />
-            Collections
-          </Button>
+          <Link href="/collections" className="hidden lg:block">
+            <Button
+              variant="default"
+              className="flex items-center text-lg font-[300]"
+            >
+              <Heart size={18} />
+              Collections
+            </Button>
+          </Link>
         </div>
       </Container>
     </header>
