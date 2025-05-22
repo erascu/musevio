32b646c6a13eeb9b5e83da85fea4e597d3bd7a91
erascu <e.rashku@gmail.com>
2025-05-22 18:56:37 +0300

    Add image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ids.lib.harvard.edu",
+        pathname: "/ids/view/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
