b99e25dbbcfead0416e0426bda477f68ccac2484
erascu <e.rashku@gmail.com>
2025-05-23 16:21:36 +0300

    Add about page component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/shared";
+import React from "react";
+
+export default function About() {
+  return (
+    <Container className="px-4">
+      <div className="w-[250px] border-b pb-3">
+        <h1 className="playfair !text-4xl font-bold">About</h1>
+      </div>
+    </Container>
+  );
+}
