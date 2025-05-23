c2b645030b423b7ee583af962f682f08ba2aac82
erascu <e.rashku@gmail.com>
2025-05-23 16:08:10 +0300

    Add BurgerMenu to export

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,4 +3,5 @@ export { Container } from "./container";
 export { Categories } from "./categories";
 export { SortPopup } from "./sort-popup";
 export { ArtworkCard } from "./artwork-card";
+export { BurgerMenu } from "./burger-menu";
 export { Footer } from "./footer";
