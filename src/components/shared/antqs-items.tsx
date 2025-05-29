06802e68c627dd339877e07e7222a282843f9415
erascu <e.rashku@gmail.com>
2025-05-29 15:21:36 +0300

    Add try/catch and typescript types

--- src/components/shared/antqs-items.tsx
+++ src/components/shared/antqs-items.tsx
@@ -1,28 +1,53 @@
 import { ArtworkCard } from "./";
-import { Antiquity } from "@/lib/types";
+import { getAntiquities } from "@/app/services/api";
 
-interface Props {
-  fetchedAntqs: Antiquity["records"];
-}
+export default async function AntqsItems({
+  page,
+  filter,
+  sort,
+  sortorder,
+  searchQuery,
+}: {
+  page: number;
+  filter: string;
+  searchQuery?: string;
+  sort: string;
+  sortorder: string;
+}) {
+  try {
+    const { fetchedAntqs } = await getAntiquities(
+      page,
+      filter,
+      sort,
+      sortorder,
+      searchQuery
+    );
+    return (
+      <>
+        {fetchedAntqs.map((item) => (
+          <ArtworkCard
+            key={item.id}
+            id={item.id}
+            section="antiquities"
+            imageUrl={
+              item?.primaryimageurl
+                ? `${item.primaryimageurl}?width=500`
+                : "no-img.svg"
+            }
+            title={item.title}
+            classification={item.classification}
+            dated={item.dated}
+          />
+        ))}
+      </>
+    );
+  } catch (error) {
+    console.error("Failed to load antiquities:", error);
 
-export default function AntqsItems({ fetchedAntqs }: Props) {
-  return (
-    <>
-      {fetchedAntqs.map((item) => (
-        <ArtworkCard
-          key={item.id}
-          id={item.id}
-          section="antiquities"
-          imageUrl={
-            item?.primaryimageurl
-              ? `${item.primaryimageurl}?width=500`
-              : "no-img.svg"
-          }
-          title={item.title}
-          classification={item.classification}
-          dated={item.dated}
-        />
-      ))}
-    </>
-  );
+    return (
+      <div className="col-span-full text-center h-[400px] flex justify-center items-center text-sm text-wine">
+        Something went wrong while loading the items.
+      </div>
+    );
+  }
 }
