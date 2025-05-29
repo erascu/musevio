717c2e7541840ffbc46866c16af22e59743f1cb8
erascu <e.rashku@gmail.com>
2025-05-29 15:28:10 +0300

    Modify and add another ts type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,7 @@ export interface Antiquity {
   info: {
     totalrecords: number;
     pages: number;
+    page: number;
   };
   records: {
     id: number;
@@ -11,4 +12,20 @@ export interface Antiquity {
     classification: string;
     dated: string | number;
   }[];
+}
+
+export interface ItemProps {
+  records: [
+    {
+      id: number;
+      title: string;
+      classification: string;
+      medium: string;
+      period: string;
+      dated: string | number;
+      department: string;
+      primaryimageurl: string;
+      description?: string;
+    }
+  ];
 }
