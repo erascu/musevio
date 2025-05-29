e5b26b759bac8738b6fa401967f3856f4f230e5c
erascu <e.rashku@gmail.com>
2025-05-29 15:19:45 +0300

    Add single item fetch and refactor list fetch with search, filters, and pagination

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,16 +1,37 @@
-import { Antiquity } from "@/lib/types";
+import { Antiquity, ItemProps } from "@/lib/types";
 
-export async function getAntiquities(page: number) {
-  const apiKey: string = "e33e0c2d-531f-4088-a7b8-7f27ccf0b27d";
+const apiKey: string = "e33e0c2d-531f-4088-a7b8-7f27ccf0b27d";
+
+export async function getAntiquities(
+  page: number,
+  filter: string,
+  sort: string,
+  sortorder: string,
+  searchQuery?: string
+) {
   const defaultFilters: string =
     "Coins|Sculpture|Vessels|Jewelry|Seals|Manuscripts";
+  const appliedFilter = filter && filter !== "All" ? filter : defaultFilters;
+  const searchParam = searchQuery
+    ? `&q=${encodeURIComponent(searchQuery)}`
+    : "";
 
   const res = await fetch(
-    `https://api.harvardartmuseums.org/object?apikey=${apiKey}&page=${page}&size=12&classification=${defaultFilters}&sort=totalpageviews&sortorder=desc`
+    `https://api.harvardartmuseums.org/object?apikey=${apiKey}&hasimage=1&page=${page}&size=12&classification=${appliedFilter}&sort=${sort}&sortorder=${sortorder}${searchParam}`
   );
   const getAntqs: Antiquity = await res.json();
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   const totalRecords = getAntqs.info.totalrecords;
+  const recordsInfo = getAntqs.info;
   const fetchedAntqs = getAntqs.records;
-  return { fetchedAntqs, totalRecords };
+  return { fetchedAntqs, totalRecords, recordsInfo };
+}
+
+export async function getItem(id: number | string) {
+  const res = await fetch(
+    `https://api.harvardartmuseums.org/object?apikey=${apiKey}&id=${id}`
+  );
+  const fetchedItem: ItemProps = await res.json();
+  const itemInfo = fetchedItem.records;
+  return itemInfo[0];
 }
