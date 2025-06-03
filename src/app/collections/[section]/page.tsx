1115ba271b2a031fb9b3271ac55e6b36a99698c8
erascu <e.rashku@gmail.com>
2025-06-03 19:22:22 +0300

    WIP: Refactor code, moved half into a new component

--- src/app/collections/[section]/page.tsx
+++ src/app/collections/[section]/page.tsx
@@ -1,16 +1,60 @@
 "use client";
-import { useParams } from "next/navigation";
+// import { useParams } from "next/navigation";
 import NotFound from "@/app/not-found";
-import { Container } from "@/components/shared";
+import {
+  // ArtworkCard,
+  Container,
+  NoCollections,
+  SavedColPanel,
+} from "@/components/shared";
+// import { useFavItemsStore } from "@/stores/favItems-store";
 import { useNewCollectionStore } from "@/stores/section-store";
 
 export default function CollectionPage() {
   const newCollection = useNewCollectionStore((state) => state.newCollection);
-  const { section } = useParams();
+  // const favItems = useFavItemsStore((state) => state.favItems);
   try {
     return (
       <Container className="px-4">
-        {newCollection.length > 0 ? <div>{section}</div> : <NotFound />}
+        {newCollection.length > 0 ? (
+          <>
+            <div className="w-[250px] border-b pb-3">
+              <h1 className="playfair !text-4xl font-bold">Collections</h1>
+            </div>
+            <div className="flex flex-col lg:flex-row">
+              <SavedColPanel />
+              <div className="flex-1 lg:pl-5 pl-0 pb-10 mt-4.5 lg:mt-0">
+                <NoCollections />
+                {/* <div
+                  className={
+                    favItems.length > 0
+                      ? "grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6"
+                      : "flex justify-center items-center min-h-[350px]"
+                  }
+                >
+                  {favItems.length > 0 ? (
+                    favItems.map((item) => (
+                      <ArtworkCard
+                        key={item.id}
+                        id={item.id ?? 0}
+                        section="antiquities"
+                        imageUrl={item.imageUrl ?? ""}
+                        title={item.title ?? ""}
+                        classification={item.classification ?? ""}
+                        dated={item.dated ?? ""}
+                        editCards
+                      />
+                    ))
+                  ) : (
+                    <NoCollections />
+                  )}
+                </div> */}
+              </div>
+            </div>
+          </>
+        ) : (
+          <NotFound />
+        )}
       </Container>
     );
   } catch (error) {
