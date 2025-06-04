ec20c47c5d514126b89c66d72ae4c4dbc755e005
erascu <e.rashku@gmail.com>
2025-06-04 17:00:46 +0300

    Add filter to show saved items on main section

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -7,9 +7,19 @@ import {
 } from "@/components/shared";
 import React from "react";
 import { useFavItemsStore } from "@/stores/favItems-store";
+import { useParams } from "next/navigation";
 
 export default function Collections() {
   const favItems = useFavItemsStore((state) => state.favItems);
+
+  const { section } = useParams<{ section: string }>();
+  const activeSection = decodeURIComponent(section || "");
+
+  const filteredItems = favItems.filter((item) =>
+    activeSection
+      ? item.newCollectionSect === activeSection
+      : !item.newCollectionSect
+  );
 
   return (
     <Container className="px-4">
@@ -21,13 +31,13 @@ export default function Collections() {
         <div className="flex-1 lg:pl-5 pl-0 pb-10 mt-4.5 lg:mt-0">
           <div
             className={
-              favItems.length > 0
+              filteredItems.length > 0
                 ? "grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6"
                 : "flex justify-center items-center min-h-[350px]"
             }
           >
-            {favItems.length > 0 ? (
-              favItems.map((item) => (
+            {filteredItems.length > 0 ? (
+              filteredItems.map((item) => (
                 <ArtworkCard
                   key={item.id}
                   id={item.id}
@@ -36,6 +46,8 @@ export default function Collections() {
                   title={item.title ?? ""}
                   classification={item.classification ?? ""}
                   dated={item.dated ?? ""}
+                  newCollectionSect={item.newCollectionSect}
+                  collection={item.collection}
                   editCards
                 />
               ))
