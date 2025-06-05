ba2f0e62d89862d3bc6d41d12f0734870da73c6e
erascu <e.rashku@gmail.com>
2025-06-05 19:39:41 +0300

    Refactor code

--- src/app/fineart/page.tsx
+++ src/app/fineart/page.tsx
@@ -1,5 +1,4 @@
 import {
-  ArtworkCard,
   Categories,
   Container,
   PaginationWithLinks,
@@ -7,8 +6,9 @@ import {
   // SortPopup,
 } from "@/components/shared";
 import React from "react";
-import { getFineArt } from "../../services/api";
+import { getFineArt } from "@/services/api";
 import NotFound from "../not-found";
+import FineArtItems from "@/components/shared/fineArt-items";
 
 const fineartCats = [
   { name: "All", filterString: "All" },
@@ -25,67 +25,51 @@ interface ItemsProps {
 }
 
 export default async function FineArt({ searchParams }: ItemsProps) {
-  try {
-    const params = await searchParams;
-    const currentPage = parseInt((params.page as string) || "1");
-    const filter = (params.filterBy as string) || "";
-    const searchQuery = (params.keyword as string) || "";
+  const params = await searchParams;
+  const currentPage = parseInt((params.page as string) || "1");
+  const filter = (params.filterBy as string) || "";
+  const searchQuery = (params.keyword as string) || "";
 
-    const { fineArtInfo, fetchedFineArt } = await getFineArt(
-      currentPage,
-      filter,
-      searchQuery
-    );
+  const { fineArtInfo, fetchedFineArt } = await getFineArt(
+    currentPage,
+    filter,
+    searchQuery
+  );
 
-    return (
-      <Container className="px-4">
-        <div className="w-[250px] border-b pb-3">
-          <h1 className="playfair !text-4xl font-bold">Fine Art</h1>
+  return (
+    <Container className="px-4">
+      <div className="w-[250px] border-b pb-3">
+        <h1 className="playfair !text-4xl font-bold">Fine Art</h1>
+      </div>
+      <div className="flex flex-col ">
+        <div className="flex justify-between items-start flex-col min-[1100px]:flex-row min-[1100px]:items-center">
+          <Categories categories={fineartCats} />
+          <SearchInput />
         </div>
-        <div className="flex flex-col ">
-          <div className="flex justify-between items-start flex-col min-[1100px]:flex-row min-[1100px]:items-center">
-            <Categories categories={fineartCats} />
-            <SearchInput />
-          </div>
-          {/* <SortPopup /> */}
-        </div>
-        {fetchedFineArt.length === 0 && fineArtInfo.total > 0 && <NotFound />}
-        <div className="pt-3 pb-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {fineArtInfo.total === 0 && searchQuery && (
-            <div className="flex justify-center items-center">
-              No &apos;{searchQuery}&apos; records found
-            </div>
-          )}
-          {fetchedFineArt.map((fineart) => (
-            <ArtworkCard
-              key={fineart.id}
-              id={fineart.id}
-              section="fineart"
-              imageUrl={fineart.images.web?.url}
-              title={fineart.title}
-              classification={fineart.type}
-              dated={fineart.creation_date}
-            />
-          ))}
-        </div>
-        {!(fineArtInfo.total === 0 || fetchedFineArt.length === 0) && (
-          <div className="pb-8">
-            <PaginationWithLinks
-              page={currentPage}
-              pageSize={12}
-              totalCount={fineArtInfo.total}
-            />
+        {/* <SortPopup /> */}
+      </div>
+      {fetchedFineArt.length === 0 && fineArtInfo.total > 0 && <NotFound />}
+      <div className="pt-3 pb-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {fineArtInfo.total === 0 && searchQuery && (
+          <div className="flex justify-center items-center">
+            No &apos;{searchQuery}&apos; records found
           </div>
         )}
-      </Container>
-    );
-  } catch (error) {
-    console.error("Failed to load fineart:", error);
-
-    return (
-      <div className="col-span-full text-center h-[400px] flex justify-center items-center text-sm text-wine">
-        Something went wrong while loading the items.
+        <FineArtItems
+          page={currentPage}
+          filter={filter}
+          searchQuery={searchQuery}
+        />
       </div>
-    );
-  }
+      {!(fineArtInfo.total === 0 || fetchedFineArt.length === 0) && (
+        <div className="pb-8">
+          <PaginationWithLinks
+            page={currentPage}
+            pageSize={12}
+            totalCount={fineArtInfo.total}
+          />
+        </div>
+      )}
+    </Container>
+  );
 }
