de5d8cd5411c8d06cabf982dbdce1342027a5bd4
erascu <e.rashku@gmail.com>
2025-06-08 17:04:28 +0300

    Modify process.env line

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,25 +2,25 @@
 import { z } from "zod";
 import { formSchema } from "./schemas";
 
-const postMessage = process.env.POST_MESSAGE;
+const postMessage = process.env.POST_MESSAGE!;
 
 export const send = async (data: z.infer<typeof formSchema>) => {
-    try {
-        const res = await fetch(postMessage!, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
+  try {
+    const res = await fetch(postMessage!, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-        if(!res.ok) {
-            throw new Error (`Failed to send data: ${res.status} ${res.statusText}`);
-        }
-
-        const result = await res.json();
-        return result;
-    } catch (err) {
-        throw err;
+    if (!res.ok) {
+      throw new Error(`Failed to send data: ${res.status} ${res.statusText}`);
     }
-}
+
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
